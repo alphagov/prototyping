6aea5f4bc04997f3f3a675ba8b6da3338ddefc15
Tim P <contact@timpaul.co.uk>
2012-06-26 17:09:56 +0100

    Update to second iteration of Tax Disc prototype

--- apply-for-tax-disc/assets/js/behaviours.js
+++ apply-for-tax-disc/assets/js/behaviours.js
@@ -5,7 +5,8 @@ $(function() {
 // =======================================================================
 
 // Toggle the hidden content when the user clicks on the visible trigger
-$("details summary").click(function() {
+//$("details > summary").not($("details details summary")).click(function() {
+$("details > summary").click(function() {
   //$(this).siblings().slideToggle(60);
   $(this).parents().toggleClass("open");
 });
