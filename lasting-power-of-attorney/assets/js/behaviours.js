0053bc6adf483c34919c38080045e2cb0b1abb5c
Tim P <contact@timpaul.co.uk>
2012-07-15 16:18:13 +0100

    OPG: More forms styles - address lookup etc.

--- lasting-power-of-attorney/assets/js/behaviours.js
+++ lasting-power-of-attorney/assets/js/behaviours.js
@@ -1,8 +1,8 @@
 $(function() {
 
 	// Radio button label styles		
-	var $radios = jQuery('.touchy input[type=radio]');
-   	$radios.filter(':checked').parent().addClass('checked');
+	var $radios = $('.emphasised input');
+  $radios.filter(':checked').parent().addClass('checked');
 	$radios.change(function() {
    		$radios.parent().removeClass('checked');
    		$radios.filter(':checked').parent().addClass('checked');
@@ -15,7 +15,7 @@ $(function() {
 
 
 	// Move to next part
-	$(".continue-button .button").click(function() {
+	$(".next-part").click(function() {
       $(this).closest(".accordion-item").removeClass('current').addClass('complete')
       .next(".accordion-item").slideDown().addClass('current');
       return false;
