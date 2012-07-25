bba6fe83c5a81c5d294edd82f53c8e8315f96326
Alex Torrance <hello@alextorrance.co.uk>
2012-07-25 15:09:48 +0100

    Add finance finder prototype

---
+++ finance-finder/assets/javascript/finance.js
@@ -0,0 +1,37 @@
+function initToggle () {
+  $('.filters').on('click', 'a.js-toggle', function (e) {
+    e.preventDefault();
+    $(this).parent().toggleClass('open');
+  });
+}
+
+$(document).ready(function() {
+  // show business sectors
+  $('#browse-sectors').on('click', function (e) {
+    e.preventDefault();
+    $(this).parent().hide();
+    $('#business-sectors').removeClass('hidden');
+  });
+
+  // add items to list
+  $('.search-picker').on('click', 'a.add', function (e) {
+    e.preventDefault();
+    var $this = $(this), $li = $this.parent('li').clone();
+    $li.find('a.add').removeClass('add').addClass('remove').text('Remove');
+
+    var $picked = $this.parents('.step').find('.picked-items');
+    $picked.find('ul').append($li).end().find('.button-container').removeClass('hidden');
+  });
+
+  // remove items from list
+  $('.picked-items').on('click', 'a.remove', function (e) {
+    e.preventDefault();
+    $(this).parents('li').remove();
+
+    if ($('.picked-items').find('li').length === 0) {
+      $('.picked-items').find('.button-container').addClass('hidden');
+    }
+  });
+
+  initToggle();
+});
