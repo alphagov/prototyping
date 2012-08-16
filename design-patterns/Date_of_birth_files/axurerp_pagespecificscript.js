8b4396b65f866c2ce9f24a7866b11f1526241efb
Tim P <contact@timpaul.co.uk>
2012-08-16 09:36:17 +0100

    Design patterns: Add alternative pattern page style

--- design-patterns/Date_of_birth_files/axurerp_pagespecificscript.js
+++ design-patterns/Date_of_birth_files/axurerp_pagespecificscript.js
@@ -1,22 +1,33 @@
-for(var i = 0; i < 64; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+for(var i = 0; i < 90; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
 
 $axure.eventManager.pageLoad(
 function (e) {
 
 });
-gv_vAlignTable['u51'] = 'top';gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u22'] = 'center';gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u27'] = 'top';gv_vAlignTable['u28'] = 'top';gv_vAlignTable['u62'] = 'center';gv_vAlignTable['u30'] = 'center';gv_vAlignTable['u34'] = 'top';gv_vAlignTable['u35'] = 'top';gv_vAlignTable['u36'] = 'top';gv_vAlignTable['u37'] = 'top';gv_vAlignTable['u39'] = 'center';gv_vAlignTable['u3'] = 'top';gv_vAlignTable['u7'] = 'top';gv_vAlignTable['u8'] = 'top';
-u40.style.cursor = 'pointer';
-$axure.eventManager.click('u40', function(e) {
+gv_vAlignTable['u81'] = 'top';gv_vAlignTable['u82'] = 'top';gv_vAlignTable['u83'] = 'top';gv_vAlignTable['u85'] = 'center';u86.tabIndex = 0;
+
+u86.style.cursor = 'pointer';
+$axure.eventManager.click('u86', function(e) {
 
 if (true) {
 
-	self.location.href=$axure.globalVariableProvider.getLinkUrl('Rationale.html');
+	self.location.href='https://gist.github.com/3360753';
 
 }
 });
-gv_vAlignTable['u45'] = 'center';gv_vAlignTable['u43'] = 'center';gv_vAlignTable['u48'] = 'top';gv_vAlignTable['u46'] = 'top';
-u47.style.cursor = 'pointer';
-$axure.eventManager.click('u47', function(e) {
+gv_vAlignTable['u88'] = 'center';
+u89.style.cursor = 'pointer';
+$axure.eventManager.click('u89', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Rationale_1.html');
+
+}
+});
+gv_vAlignTable['u12'] = 'center';gv_vAlignTable['u14'] = 'center';gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u19'] = 'center';gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u25'] = 'center';gv_vAlignTable['u27'] = 'center';gv_vAlignTable['u29'] = 'center';gv_vAlignTable['u33'] = 'top';gv_vAlignTable['u34'] = 'top';gv_vAlignTable['u36'] = 'center';gv_vAlignTable['u38'] = 'center';gv_vAlignTable['u40'] = 'center';gv_vAlignTable['u44'] = 'top';gv_vAlignTable['u46'] = 'center';gv_vAlignTable['u48'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u4'] = 'top';
+u5.style.cursor = 'pointer';
+$axure.eventManager.click('u5', function(e) {
 
 if (true) {
 
@@ -24,9 +35,9 @@ if (true) {
 
 }
 });
-
-u49.style.cursor = 'pointer';
-$axure.eventManager.click('u49', function(e) {
+gv_vAlignTable['u6'] = 'top';
+u7.style.cursor = 'pointer';
+$axure.eventManager.click('u7', function(e) {
 
 if (true) {
 
@@ -34,4 +45,4 @@ if (true) {
 
 }
 });
-gv_vAlignTable['u12'] = 'top';gv_vAlignTable['u13'] = 'top';gv_vAlignTable['u17'] = 'top';gv_vAlignTable['u18'] = 'top';gv_vAlignTable['u19'] = 'top';
+gv_vAlignTable['u9'] = 'top';gv_vAlignTable['u50'] = 'center';gv_vAlignTable['u54'] = 'top';gv_vAlignTable['u55'] = 'top';gv_vAlignTable['u57'] = 'center';gv_vAlignTable['u59'] = 'center';gv_vAlignTable['u61'] = 'center';gv_vAlignTable['u63'] = 'center';gv_vAlignTable['u65'] = 'center';gv_vAlignTable['u67'] = 'center';gv_vAlignTable['u69'] = 'center';gv_vAlignTable['u70'] = 'top';gv_vAlignTable['u74'] = 'top';gv_vAlignTable['u75'] = 'top';gv_vAlignTable['u77'] = 'center';
--- design-patterns/data/sitemap.js
+++ design-patterns/data/sitemap.js
@@ -2,7 +2,7 @@
 
 (function() {
     var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k,i,[_(c,l,e,f,g,m)])])]);}; 
-var b="rootNodes",c="pageName",d="Patterns",e="type",f="Wireframe",g="url",h="Patterns.html",i="children",j="Date of birth",k="Date_of_birth.html",l="Rationale",m="Rationale.html";
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k,i,[_(c,l,e,f,g,m)]),_(c,n,e,f,g,o,i,[_(c,l,e,f,g,p)])])]);}; 
+var b="rootNodes",c="pageName",d="Patterns",e="type",f="Wireframe",g="url",h="Patterns.html",i="children",j="Date of birth",k="Date_of_birth.html",l="Rationale",m="Rationale.html",n="Date of birth 2",o="Date_of_birth_2.html",p="Rationale_1.html";
 return _creator();
 })();
