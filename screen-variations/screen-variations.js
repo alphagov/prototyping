31bbcff398498d8034fa7ef8a37af1093cd6a91b
tombye <tom.byers@digital.cabinet-office.gov.uk>
2012-08-21 10:50:20 +0100

    Fixing the inner text of the navigation link

--- screen-variations/screen-variations.js
+++ screen-variations/screen-variations.js
@@ -2,29 +2,42 @@
   var $navbar = $('div.article-container aside nav.page-navigation'),
       $navItems = $navbar.find('ol li')
       $showAllLink = $('<a href="#" class="show-all-parts">Show all parts</a>'),
-      activeTab = $navItems.index('.active');
-
-  var $nonActiveTabs = $navItems.not('.active');
+      labelHTML = {
+        open : ['Page ', '<span class="visuallyhidden">:</span>'],
+        close : ['Page ', ' of ']
+      },
+      activeTab = $navItems.index('.active'),
+      $nonActiveTabs = $navItems.not('.active'),
+      spanTxt = $navItems.eq(activeTab).find('span.part-label').text(),
+      spanHTML = $navItems.eq(activeTab).find('span.part-label').html(),
+      alterSpan = function ($link, action) {
+        var $span = $link.find('span.part-label');
+          
+        $span.html(labelHTML[action][0] + (activeTab + 1) + labelHTML[action][1]);
+      };
 
   $showAllLink.insertBefore($navbar);
   $showAllLink.on('click', function (e) {
     var $this = $(this);
 
     if(!$this.hasClass('show-all-parts-open')) {
-      $(this).addClass('show-all-parts-open');
+      $this.addClass('show-all-parts-open');
+      alterSpan($navItems.eq(activeTab), 'open');
       $nonActiveTabs.show();
+      $navbar.addClass('page-navigation-open');
     } else {
-      $(this).removeClass('show-all-parts-open');
+      $this.removeClass('show-all-parts-open');
+      alterSpan($navItems.eq(activeTab), 'close');
       $nonActiveTabs.hide();
+      $navbar.removeClass('page-navigation-open');
     }
 
     return false;
   });
 
-  var spanTxt = $navItems.eq(activeTab).find('span.part-label').text();
   $navItems.eq(activeTab)
     .find('span.part-label')
-    .html(spanTxt.replace(/\:/,' of ' + $navItems.length + '<span class="visuallyhidden">:</span>'));
+    .html(spanTxt.replace(/\:/,' of ' + $navItems.length));
 
     $nonActiveTabs.hide();
 }(jQuery));
