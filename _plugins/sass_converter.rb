a8b5e6684dc7cc08cc5deca47978d0b9a09e7110
Tim P <contact@timpaul.co.uk>
2012-10-12 09:32:42 +0100

    Rename Sass directory to follow Rails asset naming conventions

--- _plugins/sass_converter.rb
+++ _plugins/sass_converter.rb
@@ -20,7 +20,7 @@ module Jekyll
       begin
         puts "Performing Sass Conversion."
         gem_dir = Bundler.load.specs.find{|s| s.name == 'govuk_frontend_toolkit' }.full_gem_path
-        engine = Sass::Engine.new(content, :syntax => :scss, :load_paths => ["./_includes/scss/", gem_dir + "/app/assets/stylesheets/"], :line_numbers => true)
+        engine = Sass::Engine.new(content, :syntax => :scss, :load_paths => ["./_includes/stylesheets/", gem_dir + "/app/assets/stylesheets/"], :line_numbers => true)
         engine.render
       rescue StandardError => e
         puts "!!! SASS Error - [Line #{e.sass_line}] -- #{e.to_s}"
