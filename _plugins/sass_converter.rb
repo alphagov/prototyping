be8dbdd5da52970f7b53e5e87d115f8ff760866b
Alex Torrance <hello@alextorrance.co.uk>
2012-10-30 10:36:02 +0000

    Add timestamp to Sass conversion output

--- _plugins/sass_converter.rb
+++ _plugins/sass_converter.rb
@@ -18,12 +18,13 @@ module Jekyll
 
     def convert(content)
       begin
-        puts "Performing Sass Conversion."
+        t = Time.now.strftime("%Y-%m-%d %H:%M:%S");
+        puts "[#{t}] Performing Sass Conversion."
         gem_dir = Bundler.load.specs.find{|s| s.name == 'govuk_frontend_toolkit' }.full_gem_path
         engine = Sass::Engine.new(content, :syntax => :scss, :load_paths => ["./_includes/stylesheets/", gem_dir + "/app/assets/stylesheets/"], :line_numbers => true)
         engine.render
       rescue StandardError => e
-        puts "!!! SASS Error - [Line #{e.sass_line}] -- #{e.to_s}"
+        puts "[#{t}] !!! SASS Error - [Line #{e.sass_line}] -- #{e.to_s}"
       end
     end
   end
