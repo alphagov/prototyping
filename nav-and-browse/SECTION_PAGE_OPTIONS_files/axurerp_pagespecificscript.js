78d2c1413a4462f5136bc7d2e388b46c948d16a4
Alex Torrance <hello@alextorrance.co.uk>
2013-01-30 14:36:11 +0000

    Remove old prototyping projects
    I removed all projects that hadn't been committed to in over 2 months.
    
    If there's something in here you think you will need, then let me know.

--- nav-and-browse/SECTION_PAGE_OPTIONS_files/axurerp_pagespecificscript.js
+++
@@ -1,6 +0,0 @@
-for(var i = 0; i < 0; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
-
-$axure.eventManager.pageLoad(
-function (e) {
-
-});
--- nav-and-browse/Subsection__UK_Government_files/axurerp_pagespecificscript.js
+++
@@ -1,7 +0,0 @@
-for(var i = 0; i < 50; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
-
-$axure.eventManager.pageLoad(
-function (e) {
-
-});
-gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u21'] = 'top';gv_vAlignTable['u22'] = 'top';gv_vAlignTable['u24'] = 'center';gv_vAlignTable['u27'] = 'center';gv_vAlignTable['u29'] = 'center';gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u32'] = 'top';gv_vAlignTable['u33'] = 'top';gv_vAlignTable['u35'] = 'center';gv_vAlignTable['u36'] = 'top';gv_vAlignTable['u38'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u4'] = 'top';gv_vAlignTable['u5'] = 'top';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u42'] = 'center';gv_vAlignTable['u40'] = 'center';gv_vAlignTable['u45'] = 'top';gv_vAlignTable['u43'] = 'top';gv_vAlignTable['u44'] = 'top';gv_vAlignTable['u48'] = 'top';gv_vAlignTable['u46'] = 'top';gv_vAlignTable['u47'] = 'top';gv_vAlignTable['u49'] = 'top';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u15'] = 'center';gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u19'] = 'center';
