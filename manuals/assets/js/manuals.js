45ef1e1ecf7488bf6d34d124ac60287e28d88db0
russell thorn <russell.thorn@digital.cabinet-office.gov.uk>
2014-04-23 12:48:03 +0100

    creating new manuals and adding content

---
+++ manuals/assets/js/manuals.js
@@ -0,0 +1,32 @@
+$(function() {
+  $( ".closed" ).click(function() {
+    $(this).parent().parent().find( ".hidden-content" ).toggle();
+    $(this).toggleClass( "open" );
+    return false;
+  });
+  $( ".closed-secondary" ).click(function() {
+    $(this).parent().parent().find( ".hidden-content-secondary" ).toggle();
+    $(this).toggleClass( "open" );
+    return false;
+  });
+  $( ".close-this" ).click(function() {
+    $(this).parent().parent().toggle();
+    $(this).parent().parent().parent().find( ".closed" ).toggleClass( "open" );
+    return false;
+  });
+
+ // $( ".open-all" ).click(function() {
+ //   $( ".hidden-content").toggle();
+ //   $( ".hidden-content-secondary").toggle();
+ //   $( ".open-all" ).toggle();
+ //   $( ".close-all" ).toggle();
+ //   return false;
+ // });
+ // $( ".close-all" ).click(function() {
+ //   $( ".hidden-content").toggle();
+ //   $( ".hidden-content-secondary").toggle();
+ //   $( ".open-all" ).toggle();
+ //   $( ".close-all" ).toggle();
+ //   return false;
+ // });
+});
