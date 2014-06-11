0bfe597e77910fbab8395f07bd24cf9a45b28aa3
russell thorn <russell.thorn@digital.cabinet-office.gov.uk>
2014-06-11 10:41:40 +0100

    add content and styling for finders

--- manuals/assets/js/manuals.js
+++ manuals/assets/js/manuals.js
@@ -1,7 +1,7 @@
 $(function() {
   $( ".closed" ).click(function() {
     $(this).parent().parent().parent().find( ".hidden-content" ).toggleClass( "open-content" );
-    $(this).parent().parent().parent().find( ".fade-summary" ).toggleClass( "grey" );
+    $(this).parent().parent().find( ".fade-summary" ).toggleClass( "grey" );
     $(this).toggleClass( "open-content" );
     return false;
   });
