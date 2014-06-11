45742e09f3afaf25493d7d893bc59f373bb0db38
russell thorn <russell.thorn@digital.cabinet-office.gov.uk>
2014-06-11 15:42:20 +0100

    js tweaks and styling updates to finders

--- finders/assets/finder.js
+++ finders/assets/finder.js
@@ -2,4 +2,26 @@ $(function() {
   $( ".head" ).on('click', function() {
     $(this).parent().toggleClass( "closed" );
   });
+
+  var showAndHide = function(){
+    var classes = $('input[type=checkbox]:checked').map(function(i,el){ return "li." + $(el).val() }).toArray().join(', ');
+
+
+    if(classes.length){
+      $('li.document').hide();
+      $(classes).show();
+    } else {
+      $('li.document').show();
+    }
+  };
+
+  var updateCount = function(){
+    var n = $( ".results li:visible" ).size();
+    $( "span.result-count" ).text(n);
+  };
+
+  $('input[type=checkbox]').on('change', function() {
+    showAndHide();
+    updateCount();
+  });
 });
