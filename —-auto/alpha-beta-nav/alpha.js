f7d218662176a85e1a4acc55b8018e1d357062ab
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-08-01 10:05:27 +0100

    Remove weirdly named directory
    I don't know what this directory is, but it has a stupid name so I guess
    someone has committed it by mistake. It contains an old copy of the
    entire directory tree.
    
    See a7cf40b42b90bef1bf8c52aa0152dd72c724af75 for when it was added

--- —-auto/alpha-beta-nav/alpha.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  var $alpha = $('#alpha-popup'),
-  html = $alpha.html();
-  var popup = NewPopup.popup(html, $alpha.attr('class'));
-});
--- —-auto/bold-testing/www.dev.gov.uk/frontend/frontend.js
+++
@@ -1,5 +0,0 @@
-// Frontend manifest
-
-
-
-;
